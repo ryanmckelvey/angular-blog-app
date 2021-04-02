d5839241a3de1c51a8bebecb3d7d550ec785dce3
RyanMcKelvey <ryanmckelvey98@gmail.com>
2021-04-02 17:52:53 +0100

    Responsively designed and styled Footer and Nav-bar components

--- blog-app/src/app/app.module.ts
+++ blog-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- blog-app/src/app/nav-bar/nav-bar.component.ts
+++ blog-app/src/app/nav-bar/nav-bar.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  public x: any
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  myFunction() {
+    this.x = document.getElementById("nav");
+    if (this.x.className === "nav-class") {
+      this.x.className += " responsive";
+    } else {
+      this.x.className = "nav-class";
+    }
+  }
 }
