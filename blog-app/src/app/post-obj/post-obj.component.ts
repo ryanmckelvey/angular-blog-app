0aa367e1819be7808fdef5a8516d7d425a145759
RyanMcKelvey <ryanmckelvey98@gmail.com>
2021-04-03 15:51:27 +0100

    Refactoring styles
    Updating color pallette, creating demo posts example and adjusting footer styling

--- blog-app/src/app/post-obj/post-obj.component.ts
+++ blog-app/src/app/post-obj/post-obj.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostObjComponent implements OnInit {
   header: string = "Header text"
-  content: string = "Content text here lorum ipsum doler set"
+  content: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis auteirure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   dateCreated: string = "today"
   author: string = "Ryan"
   constructor() { }
